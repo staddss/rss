08bc65f7911e1ec95a6f8fff1466e3f548abd482
Hatanaka <187852766+staddss@users.noreply.github.com>
2024-12-01 22:36:01 +0900

    Update dateRss.sh

--- script/dateRss.sh
+++ script/dateRss.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 OUTPUT_FILE="date.xml"
-GUID=$(date +"%y%m%d%H%M%S")
-TODAY_DATE=$(date +"%Y/%m/%d")
+GUID=$(date "+%y%m%d%H%M%S")
+TODAY_DATE=$(date "+%Y/%m/%d")
 TODAY_WEEK=$(date "+%a")
 LIMIT_DATE=$(date -d "+30 days" "+%Y/%m/%d")
 LIMIT_WEEK=$(date -d "+30 days" "+%a")
-ITEM_TITLE="▼ ${TODAY_DATE} (${TODAY_WEEK})"
+ITEM_TITLE="▼ ${TODAY_DATE} (${TODAY_WEEK})    →    ${LIMIT_DATE} (${LIMIT_WEEK})"
 
 cat <<EOF > "$OUTPUT_FILE"
 <?xml version="1.0" encoding="UTF-8"?>
@@ -20,7 +20,7 @@ cat <<EOF > "$OUTPUT_FILE"
     <item>
       <title>${ITEM_TITLE}</title>
       <link>https://github.com/staddss/rss</link>
-      <description>${LIMIT_DATE} (${LIMIT_WEEK})</description>
+      <description></description>
       <pubDate></pubDate>
       <guid>${GUID}</guid>
     </item>
